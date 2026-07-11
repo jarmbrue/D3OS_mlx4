43dd512bc3400e24f09d213557e6e86edf1ce406
Julius Armbrüster <julius.armbruester@hhu.de>
2026-07-11 15:49:36 +0200

    separate impl for flush_cache on aarch64

--- os/library/cpu/src/cpu_core.rs
+++ os/library/cpu/src/cpu_core.rs
@@ -1,33 +1,27 @@
 #![no_std]
 
-use core::arch::x86_64::{_mm_clflush, _mm_sfence};
-
 #[cfg(target_arch = "x86_64")]
-#[inline(always)]
-pub fn get_cache_line_size() -> usize {
-    let cpuid = raw_cpuid::CpuId::new();
-    cpuid
-        .get_cache_parameters()
-        .unwrap()
-        .next()
-        .map(|c| c.coherency_line_size())
-        .unwrap_or(64) // default to 64 bytes if unavailable
-}
-
 pub unsafe fn flush_cache(buffer: &[u8]) {
     let ptr = buffer.as_ptr();
     let len = buffer.len();
     let mut offset = 0;
-    #[cfg(target_arch = "x86_64")]
-    let cache_line_size = get_cache_line_size();
-
-    #[cfg(not(target_arch = "x86_64"))]
-    let cache_line_size = 64;
+    let cpuid = raw_cpuid::CpuId::new();
+    let cache_line_size = cpuid
+        .get_cache_parameters()
+        .unwrap()
+        .next()
+        .map(|c| c.coherency_line_size())
+        .unwrap_or(64); // default to 64 bytes if unavailable
 
     while offset < len {
-        unsafe { _mm_clflush(ptr.add(offset) as *const _) }; // flush one cache line
+        unsafe { core::arch::x86_64::_mm_clflush(ptr.add(offset) as *const _) }; // flush one cache line
 
         offset += cache_line_size;
     }
-    unsafe { _mm_sfence() }; // ensure all flushes are globally visible
+    unsafe { core::arch::x86_64::_mm_sfence() }; // ensure all flushes are globally visible
+}
+
+#[cfg(target_arch = "aarch64")]
+pub unsafe fn flush_cache(buffer: &[u8]) {
+    // todo flush_cache for aarch64
 }
