8a4a15b7571e1368cf9c5dd39b2f343d88395a11
Julius Armbrüster <julius.armbruester@hhu.de>
2026-08-03 22:55:10 +0200

    mlx4: use vmm map_identity for utils.rs & clean up unused functions

--- os/kernel/src/device/mlx4/utils.rs
+++ os/kernel/src/device/mlx4/utils.rs
@@ -1,16 +1,15 @@
 use pci_types::{Bar, ConfigRegionAccess};
-use x86_64::structures::paging::frame::PhysFrameRange;
 use x86_64::structures::paging::page::{Page, PageRange};
 use x86_64::structures::paging::{PageTableFlags, PhysFrame, Size4KiB};
 
 use crate::memory::PAGE_SIZE;
-use crate::{memory, process_manager};
+use crate::process_manager;
 use x86_64::{PhysAddr, VirtAddr};
 
 use core::mem;
 
-use alloc::slice;
 use crate::memory::vma::VmaType;
+use alloc::slice;
 
 pub type PageToFrameMapping = (MappedPages, PhysAddr);
 
@@ -25,54 +24,8 @@ pub struct PageToFrameRange {
 }
 
 impl PageToFrameRange {
-    pub fn from_frame(page_range: PageRange<Size4KiB>, start_frame: PhysFrame<Size4KiB>) -> Self {
-        let start_phys = get_physical_address(page_range.start.start_address());
-        if start_phys.is_null() || (start_frame.start_address() != start_phys) || !(start_frame.start_address().is_aligned(PAGE_SIZE as u64)) {
-            let null_page = Page::<Size4KiB>::containing_address(VirtAddr::zero());
-            let post_page_range = Page::range(null_page, null_page);
-            let post_frame = PhysFrame::<Size4KiB>::containing_address(PhysAddr::zero());
-
-            return Self {
-                mapped_pages: MappedPages { range: post_page_range },
-                start_frame: post_frame,
-            };
-        }
-
-        Self {
-            mapped_pages: MappedPages { range: page_range },
-            start_frame,
-        }
-    }
-
-    pub fn from_phy(page_range: PageRange<Size4KiB>, start: PhysAddr) -> Self {
-        let start_phys = get_physical_address(page_range.start.start_address());
-        if start_phys.is_null() || (start != start_phys) || !(start.is_aligned(PAGE_SIZE as u64)) {
-            let null_page = Page::<Size4KiB>::containing_address(VirtAddr::zero());
-            let post_page_range = Page::range(null_page, null_page);
-            let post_frame = PhysFrame::<Size4KiB>::containing_address(PhysAddr::zero());
-
-            return Self {
-                mapped_pages: MappedPages { range: post_page_range },
-                start_frame: post_frame,
-            };
-        }
-
-        Self {
-            mapped_pages: MappedPages { range: page_range },
-            start_frame: unsafe { PhysFrame::<Size4KiB>::from_start_address_unchecked(start) },
-        }
-    }
-
     pub fn is_valid(&self) -> bool {
         self.mapped_pages.non_zero() & !self.start_frame.start_address().is_null()
-    }
-
-    pub fn fetch_in_frame(&self) -> Result<(MappedPages, PhysFrame<Size4KiB>), &'static str> {
-        if !self.is_valid() {
-            return Err("Not a valid mapping -> fetching not possible");
-        }
-
-        Ok((self.mapped_pages, self.start_frame))
     }
 
     pub fn fetch_in_addr(&self) -> Result<(MappedPages, PhysAddr), &'static str> {
@@ -208,56 +161,12 @@ impl MappedPages {
     }
 }
 
-pub fn mapped_pages_from_frames(frame_range: PhysFrameRange) -> PageRange<Size4KiB> {
-    let v1 = VirtAddr::new(frame_range.start.start_address().as_u64());
-    let v2 = VirtAddr::new(frame_range.end.start_address().as_u64());
-
-    let start = Page::<Size4KiB>::from_start_address(v1).unwrap();
-    let end_exclusive = Page::<Size4KiB>::from_start_address(v2).unwrap();
-
-    PageRange::<Size4KiB> {
-        start: start,
-        end: end_exclusive,
-    }
-}
-
-pub fn v_address_align_up(start: VirtAddr, size: usize) -> VirtAddr {
-    let offset = start + size as u64;
-    offset.align_up(PAGE_SIZE as u64)
-}
-
 pub fn pages_required(bytes: usize) -> usize {
     (bytes + PAGE_SIZE - 1) / PAGE_SIZE
 }
 
-pub fn start_page_as_mut_ptr<T>(page: Page<Size4KiB>) -> *mut T {
-    page.start_address().as_mut_ptr::<T>()
-}
-
 pub fn start_page_as_ptr<T>(page: Page<Size4KiB>) -> *const T {
     page.start_address().as_ptr::<T>()
-}
-
-pub fn get_memory_of(frame: PhysFrame) -> PhysAddr {
-    frame.start_address()
-}
-
-pub fn fill_pages(fill_value: u8, for_page_raw: *mut u8, len: usize) {
-    let slc: &mut [u8] = unsafe { slice::from_raw_parts_mut(for_page_raw, len) };
-    slc.fill(fill_value);
-}
-
-pub fn copy_pages(from_slice: &[u8], tgt_page_raw: *mut u8, len: usize) {
-    let slc_sub: &mut [u8] = unsafe { slice::from_raw_parts_mut(tgt_page_raw, len) };
-    slc_sub.copy_from_slice(from_slice);
-}
-
-pub fn set_dma_flags(page_range: PageRange<Size4KiB>) {
-    process_manager()
-        .write()
-        .current_process()
-        .virtual_address_space
-        .set_flags(page_range, PageTableFlags::NO_EXECUTE | PageTableFlags::PRESENT | PageTableFlags::WRITABLE);
 }
 
 pub fn get_physical_address(addr: VirtAddr) -> PhysAddr {
@@ -279,11 +188,29 @@ pub fn pci_map_bar_mem(bar: Bar, tag: &str) -> MappedPages {
 }
 
 pub fn create_cont_mapping_with_dma_flags(frame_count: usize) -> Result<PageToFrameRange, &'static str> {
-    let memory = memory::alloc_frames(frame_count);
-    let page_range = mapped_pages_from_frames(memory);
-    set_dma_flags(page_range);
+    if frame_count == 0 {
+        return Err("frame_count must not be zero");
+    }
 
-    let pagetoframe = PageToFrameRange::from_frame(page_range, memory.start);
+    let process = process_manager().read().current_process();
+    let page_range = process.virtual_address_space.kernel_alloc_map_identity(
+        frame_count as u64,
+        PageTableFlags::NO_EXECUTE | PageTableFlags::PRESENT | PageTableFlags::WRITABLE,
+        VmaType::DeviceMemory,
+        "mlx_dma"
+    );
+
+    if page_range.is_empty() {
+        return Err("Failed to allocate pages");
+    }
+
+    // SAFETY: identity mapped
+    let phys_addr = PhysAddr::new(page_range.start.start_address().as_u64());
+
+    let pagetoframe = PageToFrameRange {
+        mapped_pages: MappedPages::from(page_range),
+        start_frame: PhysFrame::from_start_address(phys_addr).map_err(|_| "address was not aligned")?,
+    };
 
     Ok(pagetoframe)
 }
