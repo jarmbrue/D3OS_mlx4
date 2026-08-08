54e759dc7c35d7c190928f1d7c36cf0cd790dac0
Julius Armbrüster <julius.armbruester@hhu.de>
2026-08-08 14:35:37 +0200

    ibping: create cq, pd, qp

--- os/application/infiniband-diags/ibping/src/lib.rs
+++ os/application/infiniband-diags/ibping/src/lib.rs
@@ -2,10 +2,26 @@
 
 use terminal::{print, println};
 use ibverbs::devices;
+use ibverbs::ibv_qp_type::Type;
+use rdma::ibv_qp_cap;
 use runtime::*;
 
 #[unsafe(no_mangle)]
 pub fn main() {
     let devices = devices().expect("failed to get device list");
     println!("Found {:} devices!", devices.len());
+
+    if let Some(dev) = devices.iter().next() {
+        let ctx = dev.open().expect("failed to open device");
+        let cq = ctx.create_cq(10, 69).unwrap();
+        let pd = ctx.alloc_pd().unwrap();
+        let qp = pd.create_qp(&cq, &cq, Type::IBV_QPT_RC, ibv_qp_cap {
+            max_send_wr: 10,
+            max_recv_wr: 10,
+            max_send_sge: 3,
+            max_recv_sge: 3,
+            max_inline_data: 3,
+        });
+    }
+    return
 }
